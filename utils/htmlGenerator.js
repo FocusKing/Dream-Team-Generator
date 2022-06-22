f6f18a9e8a197c5951939a44c7ed069495b87734
FocusKing <104179717+FocusKing@users.noreply.github.com>
2022-06-22 02:07:38 -0400

    input more html

--- utils/htmlGenerator.js
+++ utils/htmlGenerator.js
@@ -1,5 +1,22 @@
 const fs = require ('fs');
 
+const generateLicense = (type) => {
+    let color;
+    if(type === "MPL") color = "red";
+    if(type === "GPL") color = "blue";
+    if(type === "Apache") color = "orange";
+    if(type === "MIT") color = "purple";
+    if(type === "CC") color = "yellow";
+    if(type === "BSD") color = "green";
+
+    return (
+        `
+        <h3>License</h3>
+        <img src="https://img.shields.io/badge/license-${type}-${color}" alt="badge-${type}" height="48" />
+        `
+    )
+}
+
 const generatePage = ({
     name,
     position,
