220887fe1f849b7b913a6536707a42a80d77299e
FocusKing <104179717+FocusKing@users.noreply.github.com>
2022-06-24 15:43:08 -0400

    input test files

--- index.js
+++ index.js
@@ -1,28 +1,84 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+
+// managers questions
 const questions = [{
     type: 'input',
-    message: 'What is your name?',
+    message: 'What is the team manager\'s name?',
     name: 'name',
 },
 
 {
     type: 'input',
-    message: 'What is your position?',
-    name: 'position',
-},
-
-{
-    type: 'input',
-    message: 'What is your Identification Number?',
+    message: 'What is the team manager\'s Identification Number?',
     name: 'identification',
 },
 
 {
     type: 'input',
-    message: 'What is your email address?',
+    message: 'What is the team manager\'s email?',
     name: 'email',
+},
+
+{
+    type: 'input',
+    message: 'What is the team manager\'s office number?',
+    name: 'number',
+},
+
+{
+    type: 'input',
+    message: 'Which type of team member would you like to add?',
+    choices: 'Engineer, Intern, No more team members to add',
+},
+
+{
+    type: 'input',
+    message: 'Which type of team member would you like to add?',
+    choices: 'Engineer, Intern, No more team members to add',
+},
+
+{
+    type: 'input',
+    message: 'Which type of team member would you like to add?',
+    choices: 'Engineer, Intern, No more team members to add',
+},
+
+{
+    type: 'input',
+    message: 'Which type of team member would you like to add?',
+    choices: 'Engineer, Intern, No more team members to add',
+},
+
+{
+    type: 'input',
+    message: 'Which type of team member would you like to add?',
+    choices: 'Engineer, Intern, No more team members to add',
+},
+
+{
+    type: 'input',
+    message: 'Which type of team member would you like to add?',
+    choices: 'Engineer, Intern, No more team members to add',
+},
+
+{
+    type: 'input',
+    message: 'Which type of team member would you like to add?',
+    choices: 'Engineer, Intern, No more team members to add',
+},
+
+{
+    type: 'input',
+    message: 'Which type of team member would you like to add?',
+    choices: 'Engineer, Intern, No more team members to add',
+},
+
+{
+    type: 'input',
+    message: 'Which type of team member would you like to add?',
+    choices: 'Engineer, Intern, No more team members to add',
 },
 ]
 inquirer.prompt(questions).then((answers) => {
